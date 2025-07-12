9fe69fbd955ef586cfeeba7789ea454f89fcce5e
martin-tavella <martutavella@gmail.com>
2025-07-11 22:06:32 -0300

    fix: env in emailSender

--- src/utils/emailSender.ts
+++ src/utils/emailSender.ts
@@ -7,8 +7,8 @@ const emailSender = async (subject: string, text: string) => {
   const transporter = createTransport({
     service: 'gmail',
     auth: {
-      user: process.env.EMAIL_USER!,
-      pass: process.env.EMAIL_PASS!,
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
     },
   });
 
